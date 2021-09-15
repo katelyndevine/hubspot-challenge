1ad92f039fc898aed9e338af49cf75c8aa89bebc
Katelyn Devine <katelyndevine.design@gmail.com>
2021-09-15 11:54:52 -0500

    feat: complete sessionsByUser

--- index.js
+++ index.js
@@ -25,20 +25,56 @@ function createSessionByUser(data) {
   let eventsByUser = {};
   for (let i = 0; i < data.events.length; i++) {
     let event = data.events[i];
-    // console.log("this is a single event", event);
     const { url, visitorId, timestamp } = event;
     if (!(visitorId in eventsByUser)) {
       eventsByUser[visitorId] = [];
     }
     eventsByUser[visitorId].push(event);
   }
-  // console.log("this is eventsByUser", eventsByUser);
   const sessionByUser = {};
 
   // sort each user session by timestamp to be in chronological order
   for (let user in eventsByUser) {
-    console.log(("this is user in object loop", user));
-    eventsByUser[user].sort((a, b) => b.timestamp - a.timestamp);
-    console.log("this is new eventsByUser after sorting", eventsByUser);
+    eventsByUser[user].sort((a, b) => a.timestamp - b.timestamp);
+    // console.log("this is new eventsByUser after sorting", eventsByUser);
   }
+
+  for (let user in eventsByUser) {
+    let sessionsList = [];
+    //compare current timestamp to the next one to see if they are wi 10mins
+    for (let i = 0; i < eventsByUser[user].length - 1; i++) {
+      let currentObj = eventsByUser[user][i];
+      let prevObj = eventsByUser[user][i - 1];
+      if (i === 0) {
+        const newSession = {
+          duration: 0,
+          pages: [currentObj.url],
+          startTime: currentObj.timestamp,
+        };
+        sessionsList.push(newSession);
+      } else {
+        // console.log("this is currentObj", currentObj);
+        let timeDiffInMins = (currentObj.timestamp - prevObj.timestamp) / 60000;
+        // console.log("this is timedif", timeDiffInMins);
+        if (timeDiffInMins <= 10) {
+          let currentSession = sessionsList[sessionsList.length - 1];
+          let currDuration = currentObj.timestamp - prevObj.timestamp;
+
+          currentSession.duration += currDuration;
+          currentSession.pages.push(currentObj.url);
+        } else {
+          const newSession = {
+            duration: 0,
+            pages: [currentObj.url],
+            startTime: currentObj.timestamp,
+          };
+          sessionsList.push(newSession);
+        }
+      }
+    }
+    sessionByUser[user] = sessionsList;
+  }
+
+  // console.log("this is sessionsList: ", sessionsList);
+  console.log("this is final!", sessionByUser);
 }
